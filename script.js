7bba35c1cc062c4b7b132f06e43b0821492727fa
santiagotarrab <santiagotarrab@gmail.com>
2022-06-11 22:00:48 -0300

    modif botones

--- script.js
+++ script.js
@@ -9,49 +9,6 @@ let apuestaNro = [];
 let lista = "";
 let elemento= document.body
 
-
-
-/* 
-let btnToogle2=document.getElementById("btnToogle")
-btnToogle2.addEventListener("click", cambiarFondo)
-
-localStorage.setItem (1,"prueba")
-localStorage.setItem (2," de")
-localStorage.setItem (3," storage")
-const producto1=[{id:1,producto:"harina"},{id:2,producto:"arroz"}]
-const enJSON = JSON.stringify(producto1)
-
-let listaLocalStorage = ""
-
-for (i=0; i<localStorage.length;i++) {
-listaLocalStorage+=localStorage.key(i)
-
-}
-alert(listaLocalStorage)
-
-
-console.log(enJSON)
-console.log(typeof enJSON)
-console.log(typeof producto1)
-
-localStorage.setItem("producto2",enJSON)
-
-
-
-
-function cambiarFondo(e){
-  e.preventDefault()
-
-  elemento.classList.toggle("fondoAzul")
-
-  alert("cambia")
-} */
-
-
-//
-
-
-
 let txtMenu = `INGRESE LA OPERACION QUE QUIERA REALIZAR:<br>
 1-CARGAR DINERO<br>
 2-JUGAR<br>
@@ -59,7 +16,6 @@ let txtMenu = `INGRESE LA OPERACION QUE QUIERA REALIZAR:<br>
 4-MOSTRAR APUESTAS<br>
 5-VER RESULTADOS RULETA<br>
 6-MOSTRAR RESULTADOS EN PANTALLA<br>`;
-
 class apuesta {
   constructor(apuestaDinero, apuestaNumero, resultadoRuleta) {
     this.apuestaDinero = apuestaDinero;
@@ -81,7 +37,7 @@ class apuesta {
   }
   ganancia() {
     if (this.apuestaNumero == this.resultadoRuleta) {
-      return this.apuestaDinero * 9;
+      return this.apuestaDinero * 10;
     } else {
       return -apuestaDinero;
     }
@@ -92,41 +48,89 @@ let valorBox = document.getElementById("textoForm");
 let mensajeTxt = document.getElementById("mensaje");
 let creditoTxt = document.querySelector("#credito");
 let btnAceptar = document.getElementById("btnAceptar");
-btnAceptar.addEventListener("click", menu);
+//btnAceptar.onclick = ()=>{menu()}
+let menuBotones= document.getElementById("botones")
+let codigo= "<h2>HOLA</h2>"
+
+
+function crearBotones(etiquetaDestino,codigoHtml,accion){
+let botonNuevo = document.createElement("div")
+botonNuevo.innerHTML= codigoHtml
+etiquetaDestino.appendChild(botonNuevo)
+botonNuevo.onclick= accion
+}
+
+
+crearBotones(menuBotones,"<h2>CARGAR DINERO</h2>",()=>menu2("1"))
+crearBotones(menuBotones,"<h2>JUGAR</h2>",()=>menu2("2"))
+crearBotones(menuBotones,"<h2>RETIRAR DINERO</h2>", ()=>menu2("3"))
+crearBotones(menuBotones,"<h2>MOSTRAR APUESTAS</h2>", ()=>menu2("4"))
 
 mensajeTxt.innerHTML = txtMenu;
 
-function menu() {
-  
+
+function menu2(opcion){
   mensajeTxt.innerHTML=txtMenu
   creditoTxt.innerHTML="$" + credito
-    alert("MENU")
   
-  switch (valorBox.value) {
+  switch (opcion) {
     case "1":
-      min = 1;
-      max = 1000;
-      btnAceptar.removeEventListener("click", menu);
-      btnAceptar.addEventListener("click", ingresarDinero);
       mensajeTxt.innerHTML = "INGRESE VALOR A CARGAR";
-
+      btnAceptar.onclick = ()=>{ingresarDinero(1,1000)}
       break;
 
     case "2":
       mensajeTxt.innerHTML = "INGRESE DINERO A APOSTAR";
-      min = 1;
-      max = 10;
-      btnAceptar.removeEventListener("click", menu);
-      btnAceptar.addEventListener("click", apostarDinero);
-     
+      btnAceptar.onclick = ()=>{apostarDinero(1,credito)}
       break;
 
     case "3":
-      alert("ingresado 3");
+      mensajeTxt.innerHTML = "INGRESE EL DINERO A RETIRAR";
+      btnAceptar.onclick = ()=>{retirarDinero(1,credito)}
       break;
 
     case "4":
-      alert("ingresado 4");
+      alert("EN CONSOLA SE MUESTRAN LAS APUESTAS")
+      mostrarApuestas()
+      break;
+
+    case "5":
+      alert("ingresado 5");
+      break;
+
+    case "6":
+      alert("ingresado 6");
+      break;
+    default:
+      alert("ingrese un valor correcto");
+      break;
+  }
+
+}
+
+function menu() {
+  mensajeTxt.innerHTML=txtMenu
+  creditoTxt.innerHTML="$" + credito
+  
+  switch (valorBox.value) {
+    case "1":
+      btnAceptar.onclick = ()=>{ingresarDinero(1,1000)}
+      mensajeTxt.innerHTML = "INGRESE VALOR A CARGAR";
+      break;
+
+    case "2":
+      mensajeTxt.innerHTML = "INGRESE DINERO A APOSTAR";
+      btnAceptar.onclick = ()=>{apostarDinero(1,credito)}
+      break;
+
+    case "3":
+      mensajeTxt.innerHTML = "INGRESE EL DINERO A RETIRAR";
+      btnAceptar.onclick = ()=>{retirarDinero(1,credito)}
+      break;
+
+    case "4":
+      alert("EN CONSOLA SE MUESTRAN LAS APUESTAS")
+      mostrarApuestas()
       break;
 
     case "5":
@@ -142,77 +146,44 @@ function menu() {
   }
 }
 
-function cargaCredito() {
-    alert("CARGAR DINERDO")
-  //credito+=solicitarValor(1,1000,"INGRESE LA CANTIDAD DE DINERO A CARGAR (1-1000)"
-}
-
-function ingresarDinero(e) {
-    alert("INGRESAR DINERDO")
-  
-
+function ingresarDinero(min,max) {
   if (Number(valorBox.value) >= min && Number(valorBox.value) <= max) {
     credito += Number(valorBox.value);
     creditoTxt.innerHTML = "CREDITO:  $" + credito;
-    btnAceptar.removeEventListener("click", ingresarDinero);
-    btnAceptar.addEventListener("click",menu);
+    btnAceptar.onclick = ()=>{menu()}
     mensajeTxt.innerHTML = txtMenu;
-    
   } else {
     alert("INGRESE UN VALOR CORRECTO");
     valorBox.value = "";
   }
 }
 
-function apostarDinero(e) {
-    
-    
+function apostarDinero(min,max) {
   if ((valorBox.value>=min) &&(valorBox.value<=max)) {
-
     apuestaDinero=valorBox.value
-
     valorBox.value=""
     mensajeTxt.innerHTML = "INGRESE A QUE NUMERO APOSTAR (1 - 10)";
-    min=1
-    max=credito
-
-
-    ///
-    btnAceptar.removeEventListener("click", apostarDinero);
-    btnAceptar.addEventListener("click", apostarNumero);
-    
+    btnAceptar.onclick = ()=>{apostarNumero(1,10)}
   } else {
     alert("INGRESE UN VALOR CORRECTO" + "min:" + min + ", max:" + max);
     valorBox.value = "";
   }
 }
 
-function apostarNumero(e) {
-    
-    alert("APOSTARNUMERO")
-    
- 
+function apostarNumero(min,max) {
   if (Number(valorBox.value) >= min && Number(valorBox.value) <= max) {
     apuestaNumero= valorBox.value
-    alert("apostado al " + apuestaNumero)
-
-
     resultadoRuleta=Number(getRandom(1,10))
     apuestaNro[i] = new apuesta(apuestaDinero,apuestaNumero,resultadoRuleta)
     console.log("APUESTA "+ i)
     console.log (apuestaNro[i])
-    // alert(apuestaNro[i].mostrarResultado())
+    alert(apuestaNro[i].mostrarResultado())
     saldarRonda()
-    //
+    alert("ronda saldada")
     i+=1
-
-    min=1
-    max=6
-    alert("fin ronda, ahora a menu")
-    btnAceptar.removeEventListener("click", apostarNumero);
-    btnAceptar.addEventListener("click", menu);
-    menu()
-
+    btnAceptar.onclick= ()=>{menu()}
+    mensajeTxt.innerHTML=txtMenu
+    creditoTxt.innerHTML="$" + credito
   } else {
     alert("INGRESE UN VALOR CORRECTO");
     valorBox.value = "";
@@ -220,9 +191,11 @@ function apostarNumero(e) {
 }
 
 function saldarRonda(){
-  credito-=apuestaNro[i].apuestaDinero
   if(apuestaNro[i].apuestaNumero==apuestaNro[i].resultadoRuleta){
     credito+=apuestaNro[i].apuestaDinero*10
+    creditoTxt.innerHTML="$" + credito
+  }else{
+    credito-=apuestaNro[i].apuestaDinero
   }
 }
 
@@ -230,137 +203,29 @@ function getRandom(min,max){
   return Math.round(Math.random() *(max-min)+min);
 }
 
+function mostrarApuestas(){
+  for(ap of apuestaNro) {
+    console.log(ap)
+  }
+}
 
+function retirarDinero(min, max){
+  let valor=Number(valorBox.value)
+  if (valor >= min && valor <= max) {
+    credito -= valor
+    creditoTxt.innerHTML = "CREDITO:  $" + credito
 
+    alert("HOLA")
+    alert("Retirado: $"+ valor)
+    btnAceptar.onclick = ()=>{menu()}
+    
 
+    mensajeTxt.innerHTML = txtMenu;
+    //menu()
 
+  } else {
+    alert("INGRESE UN VALOR CORRECTO");
+    valorBox.value = "";
+  }
 
-
-
-//do{
-
-//     switch(operacion){
-//         case 1:
-//         credito+=solicitarValor(1,1000,"INGRESE LA CANTIDAD DE DINERO A CARGAR (1-1000)."+`
-//         PARA VOLVER AL MENU INGRESE M`)
-//         break;
-
-//         case 2:
-//         jugar()
-//         break;
-
-//         case 3:
-//         retiro=solicitarValor(1,credito,"INGRESE LA CANTIDAD DE DINERO A RETIRAR. DISPONIBLE: $"+credito+`
-
-// PARA VOLVER AL MENU INGRESE M`)
-//         credito-=retiro
-//         alert("USTED RETIRO $"+ retiro)
-//         break;
-
-//         case 4:
-//             lista=""
-//             j=0
-
-//             for(j=0;j<i;j++){
-//             lista+="APUESTA " + j + ": " + apuestaNro[j].mostrarResultado() +`
-// `
-//             }
-
-//             alert(lista)
-//             break;
-
-//         case 5:
-//             resultadosTxt=`LISTA DE RESULTADOS
-
-// `
-
-//             const resultadosRuleta= apuestaNro.map (function (el){
-//                 return {
-//                     result:el.resultadoRuleta,
-//                     }
-
-//             }
-//             )
-
-//             for (const el of resultadosRuleta){
-//                 resultadosTxt+=("RESULTADO: " + el.result)+`
-// `
-//             }
-//             alert(resultadosTxt)
-//             // for(const el of resultadosRuleta){
-//             //     alert( el.resultadoRuleta)
-//             // }
-
-//         break;
-//     }
-// }while(operacion!="6")
-
-// function jugar(){
-//     apuestaDinero=solicitarValor(1,credito,`INGRESE EL DINERO A APOSTAR:
-//     DISPONIBLE: $` + credito+`
-
-//     PARA VOLVER AL MENU INGRESE M`)
-
-//     if(apuestaDinero!=0){
-
-//     apuestaNumero=solicitarValor(1,10,`INGRESE A QUE NUMERO APOSTAR (1 al 10)`+`
-
-//     PARA VOLVER AL MENU INGRESE M`)
-//     resultadoRuleta=Number(getRandom(1,10))
-
-// apuestaNro[i] = new apuesta(apuestaDinero,apuestaNumero,resultadoRuleta)
-
-//  let tabla=document.querySelector("#listaResultados")
-
-//  let trow = document.createElement("tr")
-//  trow.setAttribute("class","apuesta"+i)
-//  tabla.appendChild(trow)
-
-//   let tcol1 = document.createElement("td")
-//   tcol1.innerHTML = "$" + apuestaNro[i].apuestaDinero
-//   trow.appendChild(tcol1)
-
-//   let tcol2 = document.createElement("td")
-//   tcol2.innerHTML = apuestaNro[i].apuestaNumero
-//   trow.appendChild(tcol2)
-
-//   let tcol3 = document.createElement("td")
-//   tcol3.innerHTML = apuestaNro[i].resultadoRuleta
-//   trow.appendChild(tcol3)
-
-//     cont.innerHTML= `<p> DINERO APOSTADO: ${apuestaNro[i].apuestaDinero}<br>
-//  AL NUMERO ${apuestaNro[i].apuestaNumero}<br>
-//  RESULTADO ${apuestaNro[i].resultadoRuleta}</p>`
-
-// document.body.appendChild(cont)
-
-// i+=1;
-
-//     if(apuestaNumero===resultadoRuleta){
-//         alert(`EL RESULTADO DE LA RULETA ES `+ resultadoRuleta +`
-//         FELICIDADES! HA GANADO $`+apuestaDinero*10)
-//         credito+=apuestaDinero*10
-//     } else{
-//         alert(`EL RESULTADO DE LA RULETA ES `+ resultadoRuleta +`
-//         SUERTE EN EL PROXIMO INTENTO!`)
-//         credito-=apuestaDinero
-//     }
-//     }
-
-// }
-
-
-
-// function solicitarValor(min,max,text){
-//     let minV = Number(min)
-//     let maxV = Number(max)
-
-//     do{
-//     valorIngresado=prompt(text)
-//     }while(((Number(valorIngresado)>maxV || Number(valorIngresado)<minV)|| isNaN(valorIngresado)  ) && (!(valorIngresado=="M")))
-
-//     if(valorIngresado=="M"|| isNaN(valorIngresado)){
-//         valorIngresado=0;
-//     }
-//     return Number(valorIngresado)
-// }
+}
